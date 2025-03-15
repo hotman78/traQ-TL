fabdaad23f82c554f9cd90327d945cce4e3a915d
hotman78 <hotmanww@gmail.com>
2025-03-15 15:46:09 +0900

    params use any

--- src/app/api/icons/[id]/route.tsx
+++ src/app/api/icons/[id]/route.tsx
@@ -2,13 +2,8 @@ export const config = {
   runtime: "experimental-edge"
 };
 
-type IdType = {
-  params: {
-    id: string;
-  };
-};
-
-export async function GET(req: Request, { params }: IdType) {
+export async function GET(req: Request, { params }: any) {
+  console.log(params);
   const { id } = await params;
   // const SERVER = "https://q-dev.trapti.tech";
   const SERVER = "http://host.docker.internal:3000";
